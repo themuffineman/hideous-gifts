1a4de3b69b66e179365beae6f88c0d166aeb963d
Petrus <131905449+themuffineman@users.noreply.github.com>
2024-07-10 20:00:33 +0000

    Minor edits

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 fetch('https://hideous-gifts.onrender.com/api/generate-image',{
-  method:"POST",
+  method: "POST",
   body:JSON.stringify({
-    "uploadedImage":"https://i.ibb.co/ccnh2Rx/photo-1494790108377-be9c29b29330.jpg",
-    "targetImage":"https://i.ibb.co/y45rCdb/j-Ts0-Mw2-FQBCCIf-Jf-Da-Ihkg.webp"
+    "uploadedImage": "https://i.ibb.co/ccnh2Rx/photo-1494790108377-be9c29b29330.jpg",
+    "targetImage": "https://i.ibb.co/y45rCdb/j-Ts0-Mw2-FQBCCIf-Jf-Da-Ihkg.webp"
   })
 }).then( async (result)=>{
   const response = await result.json()
