62ae3b16f3ec90b61446449554e2a55ebd14f47f
Petrus <131905449+themuffineman@users.noreply.github.com>
2024-07-10 17:26:32 +0000

    Added json parsing

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ fetch('https://hideous-gifts.onrender.com/api/generate-image',{
     "uploadedImage":"https://i.ibb.co/ccnh2Rx/photo-1494790108377-be9c29b29330.jpg",
     "targetImage":"https://i.ibb.co/y45rCdb/j-Ts0-Mw2-FQBCCIf-Jf-Da-Ihkg.webp"
   })
-}).then((result)=>{
-  const response = JSON.parse(result)
+}).then( async (result)=>{
+  const response = await result.json()
   console.log(response)
 })
